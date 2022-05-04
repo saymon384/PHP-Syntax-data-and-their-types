ad528fbb1e57dd09bfc2e703c74d875447aadc8d
saymon384 <sanya-semonenko_12@i.ua>
2022-05-04 15:47:39 +0300

    Create idex.php

---
+++ task.loc/idex.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'Олександр';
+$last_name = 'Семененко';
+$age = '26';
+
+echo 'Мене звуть ';
+echo $name;
+echo ' ';
+echo $last_name;
+echo ', мені ';
+echo $age;
+echo ' років.';
+
+?>
